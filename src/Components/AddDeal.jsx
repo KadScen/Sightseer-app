cd41402fc4dfdc0e3373359b740514b446d88801
Alain <alain.mondely@gmail.com>
2021-02-21 11:06:10 -0500

    Add a deal structure completed

--- src/Components/AddDeal.jsx
+++ src/Components/AddDeal.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Select from 'react-select';
+
+import './AddDeal.css';
 
 export default function AddDeal() {
-
+    const options = [
+        {value:'hiking', label:'Hiking'}, 
+        {value:'sales', label:'Sales'}, 
+        {value:'traveling', label:'Traveling'}, 
+        {value:'other', label:'Other'}, 
+    ]
     return (
         <div className="addDealComponent">
             <div className="loginDiv">
@@ -13,12 +21,14 @@ export default function AddDeal() {
                         {/* <!-- Signup Form --> */}
                         <form>
                             <div className="inputField">
-                                <input type="text" id="signup-name" className="fadeIn second" name="name" placeholder="name*" required />
-                                <input type="email" id="signup-email" className="fadeIn second" name="email" placeholder="email*" required />
-                                <input type="password" id="signup-password" className="fadeIn third" name="password" placeholder="password*" required />
-                                <input type="textarea" id="signup-houseName" className="fadeIn third" placeholder="Name it here*" required />
+                                <input type="text" id="dealName" className="fadeIn second" name="dealName" placeholder="Deal name*" required />
+                                <input type="text" id="dealLocation" className="fadeIn second" name="dealLocation" placeholder="Deal location*" required />
+                                <Select placeholder="Type of deal...*" options={options} />
+                                <input type="textarea" id="dealDescription" className="fadeIn third" placeholder="Description of the deal" required />
+                                <input type="number" id="dealPrice" className="fadeIn third" placeholder="Price" required />
+                                <button>Upload photo(s)</button>
                             </div>
-                            <input type="submit" id="submitSignup" className="fadeIn fourth" value="Sign Up" />
+                            <input type="submit" id="submitSignup" className="fadeIn fourth" value="Share your deal!!" />
                         </form>
                         
                     </div>
--- src/Components/SignInUp.jsx
+++ src/Components/SignInUp.jsx
@@ -108,7 +108,6 @@ function SignInUp() {
                                 <input type="text" id="signup-name" className="fadeIn second" name="name" placeholder="name*" required />
                                 <input type="email" id="signup-email" className="fadeIn second" name="email" placeholder="email*" required />
                                 <input type="password" id="signup-password" className="fadeIn third" name="password" placeholder="password*" required />
-                                <input type="textarea" id="signup-houseName" className="fadeIn third" placeholder="Name it here*" required />
                             </div>
                             <input type="submit" id="submitSignup" className="fadeIn fourth" value="Sign Up" />
                         </form>
